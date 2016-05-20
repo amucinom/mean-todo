cb2af18564f8548ae8d2e41be9b1e734157b30e5
Aldo Mucino <amucinom@gmail.com>
2016-05-19 23:34:32 -0700

    Fixed a bug where angular would not find module

--- public/core.js
+++ public/core.js
@@ -1,42 +1,41 @@
-var scotchTodo = angular
-	.module('scotchTodo, []')
-	.controller('mainController', mainController);
+(function() {
+	var todoapp = angular.module('scotchTodo', []);
+	todoapp.controller('mainController', function ($scope, $http) {
+		$scope.formData = {};
 
-function mainController($scope, $http) {
-	$scope.formData = {};
-
-	// when landing on the page, get all todos and show them
-	$http.get('/api/todos')
-		.success(function(data) {
-			$scope.todos = data;
-			console.log(data);
-		})
-		.error(function(data) {
-			console.log("Error: " + data);
-		});
-
-	// when submitting add form, send tet to the node API
-	$scope.createTodo = function() {
-		$http.post('/api/todos', $scope.formData)
-			.success(function(data) {
-				$scope.formData = {}; // clear form
-				$scope.todos = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-	};
-
-	// delete a todo after checking it
-	$scope.deleteTodo = function(id) {
-		$http.delete('/api/todos/' + id)
+		// when landing on the page, get all todos and show them
+		$http.get('/api/todos')
 			.success(function(data) {
 				$scope.todos = data;
 				console.log(data);
 			})
 			.error(function(data) {
-				console.log('Error: ' + data);
+				console.log("Error: " + data);
 			});
-	};
-}
+
+		// when submitting add form, send tet to the node API
+		$scope.createTodo = function() {
+			$http.post('/api/todos', $scope.formData)
+				.success(function(data) {
+					$scope.formData = {}; // clear form
+					$scope.todos = data;
+					console.log(data);
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+		};
+
+		// delete a todo after checking it
+		$scope.deleteTodo = function(id) {
+			$http.delete('/api/todos/' + id)
+				.success(function(data) {
+					$scope.todos = data;
+					console.log(data);
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+		};
+	});
+})();
